1183313d22f5e318b137e2fafc7c56a468f7bf3a
Eliezer Ed Campos Laux <30839978+eliezercampos@users.noreply.github.com>
2023-07-26 00:13:18 -0600

    Ejercicios iteraciones

---
+++ for.py
@@ -0,0 +1,14 @@
+# for i in 1, 2, 3 :
+#     print(i)
+
+# for i in range(3):
+#     print(i)
+
+# for i in ["Ale", "Ivan", "Monse", "Luis", "Rafa", "Luca"] :
+#     print(i)
+
+for i in "Hola Mundo" : 
+    if i == "M" :
+        pass
+    else :
+        print(i, end=" ")
