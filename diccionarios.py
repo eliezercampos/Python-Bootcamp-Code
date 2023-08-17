650a465b1238cca2546013ce850d8ff9c772f6fb
Eliezer Ed Campos Laux <30839978+eliezercampos@users.noreply.github.com>
2023-08-16 20:36:51 -0600

    Mas ejercicios

---
+++ diccionarios.py
@@ -0,0 +1,25 @@
+# tiempos = {
+#     "Hamilton": "1:49.8",
+#     "Perez": "1:53.8",
+#     "Bottas": "1:56.3",
+#     "Verstappen": "1:52.6"
+# }
+
+# print(tiempos.items())
+
+# print(tiempos.keys())
+
+# print(tiempos.values())
+
+# print(tiempos.get("Perez"))
+
+# print(tiempos.get("Peres", "No encontrado"))
+
+tiempos1 = dict(
+    Hamilton = "1:49.8",
+    Perez = "1:53.8",
+    Bottas = "1:53.8",
+    Verstappen = "1:52.6"
+)
+
+print(tiempos1)
